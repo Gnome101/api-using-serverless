9f2f7b7bb2e7c0390598ac7ffd5c7ad14b5e5e16
Gnome101 <thegnome@FurryBeast>
2023-09-28 17:51:17 -0700

    plz

--- netlify/functions/movie-by-slug.js
+++ netlify/functions/movie-by-slug.js
@@ -1,11 +1,12 @@
-const movies = require('./data.json');
+const movies = require("./data.json");
 
-exports.handler = async (event) => {
-  const slug = event.path.replace('/api/movies/', '');
+exports.handler = async (event, context) => {
+  const slug = event.path.replace("/api/movies/", "");
+  console.log(context);
   const movie = movies.find((m) => m.slug === slug);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(movie),
+    body: JSON.stringify({ message: "Hello World" }),
   };
 };
